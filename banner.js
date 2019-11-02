45d026f165d0ac12d10c25aaa36d34e9be5bd8e5
serhademirkan <55773410+serhademirkan@users.noreply.github.com>
2019-11-02 13:59:28 +0300

    Add files via upload

---
+++ banner.js
@@ -0,0 +1,14 @@
+$(function(){
+  $('.clearbanner').click(function(){
+    $('.banner').hide();
+  });
+});
+
+/** wordpress
+jQuery(function(){
+jQuery('.clearbanner').click(function(){
+jQuery('.banner').hide();
+});
+});
+
+****/
